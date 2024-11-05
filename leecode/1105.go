ff0a4a6404a338a9a3f7d0a224d3c716960aff72
denglei <denglei1024@foxmail.com>
2024-11-05 18:05:03 +0800

    backup

---
+++ leecode/1105.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	reversedHalf := 0
+	for x > reversedHalf {
+		reversedHalf = reversedHalf*10 + x%10
+		x /= 10
+	}
+	return x == reversedHalf || x == reversedHalf/10
+}
+
+func main() {
+	fmt.Printf("%v", isPalindrome(1221))
+}
